cdb9a63490b99d007398a63702850e8557bacb31
ghwilliams <dwilliams.lima@gmail.com>
2024-09-10 13:59:16 -0300

    Adding law of motion dcp_L2.

--- dcp/motionlaws.py
+++ dcp/motionlaws.py
@@ -78,18 +78,44 @@ class dcp_L1:
         return self.D3L(t)
 
 
-### alves etal: Relativistic bands in the spectrum of created particles
-# in a oscillatory cavity.
-#
-# Equation 21
-#
-class L2:
-    def __init__(self, L0, a):
+##
+# Law of motion from article:
+#  A computer algebra package for calculation of the energy density
+#  produced via the dynamical Casimir effect in
+#  one-dimensional cavities. 2014.
+#  Danilo T. Alves a, Edney R. Granhen
+class dcp_L2:
+    def __init__(self, L0, e):
         self.L0 = L0
-        self.a = a
+        self.e = e
 
-    def __call__(self, t):
-        if t <= 0 or t > 2*self.L0:
-            return self.L0
+        ##
+        # Defines some symbold necessary to define the law of motion
+        L0_, e_, t_ = sympy.symbols('L0_ e_ t_')
+        
+        ##
+        # Defines the law of motion expression
+        L = L0_ + e_*sympy.ln(sympy.cosh(t_))
+        L = L.subs([(L0_, L0), (e_, e)])
 
-        return self.L0 + self.a * math.sin(2 * math.pi * t)
+        ##
+        # Computes derivatives of L w.r.t time
+        L_diff  = sympy.diff(L, t_)      # 1st derivative
+        L_diff2 = sympy.diff(L, t_, 2)   # 2nd derivative
+        L_diff3 = sympy.diff(L, t_, 3)   # 3rd derivative
+
+        self.DL  = sympy.utilities.lambdify(t_, L_diff)
+        self.D2L = sympy.utilities.lambdify(t_, L_diff2)
+        self.D3L = sympy.utilities.lambdify(t_, L_diff3)  
+
+    def __call__(self, t): 
+        return self.L0 + self.e * math.log(math.cosh(t))
+
+    def DL(self, t):
+        return self.DL(t)
+
+    def D2L(self, t):        
+        return self.D2L(t)
+
+    def D3L(self, t):
+        return self.D3L(t)
